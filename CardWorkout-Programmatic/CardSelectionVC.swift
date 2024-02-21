c1974e65487a8764c2cdbd37cc4653ca9426fb12
Vaibhav Ranga <vaibhavranga2@gmail.com>
2024-02-22 00:05:31 +0530

    configured button

--- CardWorkout-Programmatic/CardSelectionVC.swift
+++ CardWorkout-Programmatic/CardSelectionVC.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class CardSelectionVC: UIViewController {
+    
+    let cardImageView = UIImageView()
+    let stopButton = CWButton(backgroundColor: .red, title: "Stop!")
+    let restartButton = CWButton(backgroundColor: .green, title: "Restart")
+    let rulesButton = CWButton(backgroundColor: .blue, title: "Rules")
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .green
+        view.backgroundColor = .systemBackground
+    }
+    
+    func configureUI() {
+        
+    }
+    
+    func configureCardImageView() {
+        view.addSubview(cardImageView)
     }
 }
